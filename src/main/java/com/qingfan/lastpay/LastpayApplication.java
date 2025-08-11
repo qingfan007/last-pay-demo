b4a36b509b862538ee47d817fd9f97ab61df48b2
crystal <zdd@crystaldeMacBook-Pro.local>
2025-08-11 13:54:21 +0800

    Initial

---
+++ src/main/java/com/qingfan/lastpay/LastpayApplication.java
@@ -0,0 +1,13 @@
+package com.qingfan.lastpay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LastpayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LastpayApplication.class, args);
+    }
+
+}
